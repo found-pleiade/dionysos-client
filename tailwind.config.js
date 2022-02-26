c76a64a014371f41ec15aff13d4f55cf576b60dd
Vazanoir <vazanoir@outlook.fr>
2022-02-26 23:04:24 +0100

    Add vin color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
     './src/**/*.{ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vin: {
+          300: '#CD88AF',
+          400: '#AA5585',
+          500: '#882D60',
+          600: '#661141',
+          700: '#440026',
+        },
+      },
+    },
   },
   plugins: [],
 };
