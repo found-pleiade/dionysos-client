bfdd69ec47ee4fbba2e505ed499b3db176d9eb4b
Vazanoir <vazanoir@outlook.fr>
2022-02-26 21:55:47 +0100

    Add eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,28 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Bonjour Vite + React!</p>
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+          <button type="button" onClick={() => setCount(() => count + 1)}>
+            count is:
+            {' '}
+            {count}
           </button>
         </p>
         <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
+          Edit
+          {' '}
+          <code>App.tsx</code>
+          {' '}
+          and save to test HMR updates.
         </p>
         <p>
           <a
@@ -39,7 +45,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
