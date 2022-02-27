517adc1e7c01eab8ba9969bf7e2059bceed04e25
Vazanoir <vazanoir@outlook.fr>
2022-02-27 05:39:04 +0100

    Bigger input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -9,8 +9,7 @@ const Input = ({ value, setValue }: inputProps) => (
   <div className="mb-8 last:mb-0">
     <input
       tabIndex={0}
-      className="cursor-text transition-colors ease-out bg-zinc-200 hover:bg-zinc-100 text-zinc-500 hover:text-zinc-600 px-2
-         py-1 rounded-md flex items-center justify-between w-full"
+      className="cursor-text transition-colors ease-out bg-zinc-200 hover:bg-zinc-100 text-zinc-500 hover:text-zinc-600 px-3 py-2 rounded-md flex items-center justify-between w-full"
       type="text"
       value={value}
       onChange={(event) => setValue(event.target.value)}
