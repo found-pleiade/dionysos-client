c562ce5ce3208249ab95d5ed43b7691d5e25a1d9
Vazanoir <vazanoir@outlook.fr>
2022-02-27 05:39:21 +0100

    Validation for username

--- src/pages/connect.tsx
+++ src/pages/connect.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
+import * as R from 'ramda';
 import Button from '../components/Button';
 import Input from '../components/Input';
 
@@ -7,18 +9,28 @@ type connectProps = {
   setUsername: React.Dispatch<React.SetStateAction<string>>
 }
 
-const Connect = ({ username, setUsername }: connectProps) => (
-  <div className="bg-neutral-800 h-[250px] w-[500px] rounded-lg relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-between flex-col p-6">
-    <h1 className="text-center font-medium text-2xl">Welcome to Dyonisos</h1>
-    <div>
-      <h2 className="mb-2">Choose your username</h2>
-      <Input value={username} setValue={setUsername} />
+const gtZero = R.gt(R.__, 0);
+
+const ltFifty = R.lt(R.__, 50);
+
+const isValid = R.allPass([gtZero, ltFifty]);
+
+const Connect = ({ username, setUsername }: connectProps) => {
+  const isValidUsername = isValid(R.length(username));
+
+  return (
+    <div className="bg-neutral-800 h-[270px] w-[500px] rounded-lg relative top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-between flex-col p-6">
+      <h1 className="text-center font-medium text-2xl">Welcome to Dyonisos</h1>
+      <div>
+        <h2 className="mb-2">Choose your username</h2>
+        <Input value={username} setValue={setUsername} />
+      </div>
+      <div className="flex justify-between">
+        <div />
+        <Button to="/home" text="Join" colorless={!isValidUsername} disabled={!isValidUsername} />
+      </div>
     </div>
-    <div className="flex justify-between">
-      <div />
-      <Button to="/home" text="Join" />
-    </div>
-  </div>
-);
+  );
+};
 
 export default Connect;
