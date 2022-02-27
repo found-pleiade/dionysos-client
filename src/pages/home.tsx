0c0d55c080eecec5eb7388a4326dad5726ccdac9
Vazanoir <vazanoir@outlook.fr>
2022-02-27 23:43:27 +0100

    Basic pannel, display usern ame and id with copy

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Videojs from '../components/Videojs';
 
-const Home = () => {
+type homeProps = {
+  username: string,
+  userid: string
+}
+
+const Home = ({ username, userid }: homeProps) => {
   const playerRef = React.useRef(null);
 
   const videoJsOptions = {
-    autoplay: true,
+    autoplay: false,
     controls: true,
+    fullscreen: true,
     height: window.innerHeight,
     techOrder: ['youtube'],
     sources: [{
@@ -29,7 +35,24 @@ const Home = () => {
   };
 
   return (
-    <div className="h-screen w-screen truncate flex justify-center bg-black">
+    <div className="h-screen w-screen truncate bg-black flex">
+      <div className="flex flex-col justify-between p-3 bg-neutral-900">
+        <h1 className="text-2xl mb-20">Salon</h1>
+
+        <ul className="h-full">
+          <li>user</li>
+          <li>user</li>
+          <li>user</li>
+          <li>user</li>
+          <li>user</li>
+        </ul>
+
+        <div>
+          <p className="text-md text-neutral-100">{username}</p>
+          <button className="text-sm text-neutral-400 cursor-pointer" title="Click to copy" onClick={() => navigator.clipboard.writeText(userid)} type="button">{userid}</button>
+        </div>
+      </div>
+
       <Videojs
         options={videoJsOptions}
         onReady={handlePlayerReady}
