d4354bdfc459287c0c06ca3ab64d75a26bb3e03a
Vazanoir <vazanoir@outlook.fr>
2022-02-27 06:44:50 +0100

    Warn unused vars, ignore args

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,6 @@ module.exports = {
     'import/extensions': ['error', 'never'],
     'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
     'react/require-default-props': 0,
+    'no-unused-vars': ['warn', { vars: 'all', args: 'none', ignoreRestSiblings: false }],
   },
 };
