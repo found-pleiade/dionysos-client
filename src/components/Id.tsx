eb372013dc1e983f1f0a9359dc31ca33dc841f00
Vazanoir <vazanoir@outlook.fr>
2022-02-28 01:24:31 +0100

    Replace active to hover in Id

--- src/components/Id.tsx
+++ src/components/Id.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Id = ({ userid }: { userid: string }) => (
-  <button className="text-sm text-neutral-400 cursor-pointer active:text-neutral-500" title="Click to copy" onClick={() => navigator.clipboard.writeText(userid)} type="button">{userid}</button>
+  <button className="text-sm text-neutral-400 cursor-pointer  hover:text-neutral-300/90" title="Click to copy" onClick={() => navigator.clipboard.writeText(userid)} type="button">{userid}</button>
 );
 
 export default Id;
