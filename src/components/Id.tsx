26c661e8ece09813eb65006f7d656e3fa5f06b1c
Vazanoir <vazanoir@outlook.fr>
2022-03-05 21:46:12 +0100

    Add a clipboard icon to Id

--- src/components/Id.tsx
+++ src/components/Id.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { ClipboardIcon } from '@heroicons/react/solid';
 
 const Id = ({ id }: { id: string }) => (
-  <button className="text-sm text-neutral-400 cursor-pointer  hover:text-neutral-300/90" title="Click to copy" onClick={() => navigator.clipboard.writeText(id)} type="button">{id}</button>
+  <button className="text-sm text-neutral-400 cursor-pointer hover:text-neutral-300/90 flex" title="Click to copy" onClick={() => navigator.clipboard.writeText(id)} type="button">
+    {id}
+    {' '}
+    <ClipboardIcon className="ml-2 w-5 h-5 items-center" />
+  </button>
 );
 
 export default Id;
