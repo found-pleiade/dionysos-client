140b8f23135c8b8f1c125300326e39f1112943d1
Vazanoir <vazanoir@outlook.fr>
2022-03-06 18:41:47 +0100

    Add minizable panel

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,9 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import * as R from 'ramda';
 import { window as tauriWindow } from '@tauri-apps/api';
-import { PencilAltIcon } from '@heroicons/react/solid';
+import {
+  PencilAltIcon, ChevronLeftIcon, ChatAltIcon, MenuIcon,
+} from '@heroicons/react/solid';
 import Id from '../components/Id';
 import Userlist from '../components/Userlist';
 import Videojs from '../components/Videojs';
@@ -35,6 +37,17 @@ const joinRoom = (
   send(requestData(
     codes.request.joinRoom,
     { salt: user.salt, roomid: roomidEl.value },
+  ));
+};
+
+const changeUsername = (
+  send: sendFunction,
+  user: User,
+  newUserName: string,
+) => {
+  send(requestData(
+    codes.request.changeUserName,
+    { newUserName, salt: user.salt },
   ));
 };
 
@@ -85,6 +98,10 @@ const Home = ({
   const roomNotEmpty = room.name !== '' && room.id !== '';
   const emptyUserList = users.length <= 0;
 
+  const [panel, setPanel] = useState(true);
+  const [chat, setChat] = useState(true);
+  const translate = (condition: boolean) => (condition ? 'max-w-[650px] px-3 overflow-visible' : 'max-w-0 px-0 overflow-hidden');
+
   // document.querySelector('.vjs-fullscreen-control')?.addEventListener('click', async (event) => {
   //   const current = tauriWindow.getCurrent();
   //   if (R.not(R.isNil(current))) current.setFullscreen(!(await current.isFullscreen()));
@@ -92,7 +109,10 @@ const Home = ({
 
   return (
     <div className="h-screen w-screen truncate bg-black flex">
-      <div className="flex flex-col justify-between p-3 bg-neutral-900">
+      {/* Panel */}
+      <div className={`flex flex-col justify-between bg-neutral-900 relative transition-all py-3 ${translate(panel)}`}>
+        <ChevronLeftIcon className="block absolute top-[50%] right-3 z-10 p-1 h-8 w-8 bg-neutral-700 rounded-full cursor-pointer text-center translate-y-[-50%]" onClick={() => setPanel(false)} />
+
         <div className={`flex flex-col gap-3 ${visibility(!roomNotEmpty)}`}>
           <RoomInput text="Create" placeholder="Enter a room name" onClick={() => newRoom(send, user, room, setRoom, false)} onChange={(event: React.ChangeEvent<HTMLInputElement>) => onRoomInputChange(event, room, setRoom)} buttonClassName="bg-neutral-600 focus:bg-neutral-500 hover:bg-neutral-500" />
 
@@ -111,19 +131,37 @@ const Home = ({
         <Separator className={visibility(!emptyUserList)} />
 
         <div>
-          <p className="text-md flex items-center">
+          <button className="text-md flex items-center" onClick={() => changeUsername(send, user, 'Michel')} type="button">
             {user.name}
             {' '}
             <PencilAltIcon className="ml-2 h-5 w-4" />
-          </p>
+          </button>
           <Id id={user.id} />
         </div>
       </div>
 
-      <Videojs
-        options={videoJsOptions}
-        onReady={handlePlayerReady}
-      />
+      {/* Chat */}
+      <div className={`w-[500px] flex flex-col justify-between bg-neutral-800 relative transition-all py-3 ${translate(chat)}`}>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+
+        <ChevronLeftIcon className="block absolute top-[50%] right-3 z-10 p-1 h-8 w-8 bg-neutral-700 rounded-full cursor-pointer text-center translate-y-[-50%]" onClick={() => setChat(false)} />
+      </div>
+
+      {/* Video */}
+      <div className="relative">
+        {/* Menus */}
+        <div className="absolute top-[50%] left-3 z-10 space-y-3 translate-y-[-50%]">
+          <MenuIcon className={`block p-2 h-8 w-8 bg-neutral-700 rounded-full cursor-pointer text-center opacity-60 hover:opacity-100 transition-opacity ${visibility(!panel)}`} onClick={() => setPanel(true)} />
+
+          <ChatAltIcon className={`block p-2 h-8 w-8 bg-neutral-700 rounded-full cursor-pointer text-center opacity-60 hover:opacity-100 transition-opacity ${visibility(!chat)}`} onClick={() => setChat(true)} />
+        </div>
+
+        {/* Player */}
+        <Videojs
+          options={videoJsOptions}
+          onReady={handlePlayerReady}
+        />
+      </div>
     </div>
   );
 };
