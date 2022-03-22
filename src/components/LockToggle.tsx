72862cfe39c6dc85afb8f6d584a19c014a760575
Vazanoir <vazanoir@outlook.fr>
2022-03-22 11:41:45 +0100

    Fix accessibility on lock toggle

--- src/components/LockToggle.tsx
+++ src/components/LockToggle.tsx
@@ -4,27 +4,19 @@ import React from 'react';
 type EyeToggleProps = { toggle: boolean, onClick: any, onKeyPress: any };
 
 const LockToggle = ({ toggle, onClick, onKeyPress }: EyeToggleProps) => {
-  const style = 'px-2 h-10 w-11 cursor-pointer bg-background-500 hover:bg-background-400 text-foreground transition-colors';
+  const style = 'px-2 h-10 w-8 cursor-pointer bg-background-500 hover:bg-background-400 text-foreground transition-colors flex justify-center align-center';
 
-  const lockOpen = (
-    <LockOpenIcon
+  return (
+    <div
       className={style}
-      tabIndex={0}
       onClick={onClick}
       onKeyPress={onKeyPress}
-    />
-  );
-
-  const lockClosed = (
-    <LockClosedIcon
-      className={style}
       tabIndex={0}
-      onClick={onClick}
-      onKeyPress={onKeyPress}
-    />
+      role="button"
+    >
+      {toggle ? <LockOpenIcon className="text-accent-400" /> : <LockClosedIcon />}
+    </div>
   );
-
-  return toggle ? lockOpen : lockClosed;
 };
 
 export default LockToggle;
