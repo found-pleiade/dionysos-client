31de4443b84bdad907b038287c7bff5d9b6780ec
Vazanoir <vazanoir@outlook.fr>
2022-03-22 08:34:25 +0100

    Add more colors and shades

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,20 @@ module.exports = {
       colors: {
         foreground: '#fbfbfb',
         background: {
+          400: '#30304b',
+          500: '#28283e',
           600: '#202031',
           700: '#181825',
           800: '#101019',
           900: '#08080d',
         },
-        accent: '#9D2B84',
+        accent: {
+          400: '#b03094',
+          500: '#9D2B84',
+        },
         valid: '#72b01d',
         error: '#d62839',
+        pending: '#1C7CB0',
       },
     },
   },
