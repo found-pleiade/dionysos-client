84bab2660d3303f24aa7c66d2545a4c12a8e5492
Vazanoir <vazanoir@outlook.fr>
2022-03-23 06:39:52 +0100

    Use new animations and fix the input limit to 20

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -6,12 +6,12 @@ export const visibility = (condition: boolean) => (condition ? 'visible' : 'hidd
 
 const gtZero = (x: string) => R.gt(R.length(x), 0);
 
-const ltFifty = (x: string) => R.lt(R.length(x), 50);
+const lteTwenty = (x: string) => R.lte(R.length(x), 20);
 
 const notNil = (x: string) => R.not(R.isNil(x));
 
-export const isValid = R.allPass([gtZero, ltFifty, notNil]);
+export const isValid = R.allPass([gtZero, lteTwenty, notNil]);
 
-export const translate = (condition: boolean) => (condition ? 'max-w-[650px] px-3 overflow-visible' : 'max-w-0 px-0 overflow-hidden');
+export const translate = (condition: boolean) => (condition ? 'openSideMenus' : 'closeSideMenus');
 
 export const testActiveElementById = (id: string) => document.activeElement?.id === id;
