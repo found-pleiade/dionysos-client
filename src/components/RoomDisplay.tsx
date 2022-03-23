4cfbaf27349922375a0ad27951a98a398f017083
Vazanoir <vazanoir@outlook.fr>
2022-03-23 20:33:16 +0100

    Add a title to the logout button

--- src/components/RoomDisplay.tsx
+++ src/components/RoomDisplay.tsx
@@ -20,7 +20,9 @@ const RoomDisplay = ({
 }: RoomDisplayProps) => (
   <div className={className}>
     <h1 className="text-2xl font-black mb-3 mt-3">{room.name}</h1>
-    <LogoutIcon className="h-11 w-11 p-3 absolute right-0 top-3 bg-background-500 hover:bg-background-400 rounded-l-lg cursor-pointer transition-colors" onClick={() => quitRoom(send, user)} tabIndex={0} onKeyPress={(event) => { if (event.code === 'Enter') quitRoom(send, user); }} />
+    <div role="button" className="h-11 w-11 p-3 absolute right-0 top-3 bg-background-500 hover:bg-background-400 rounded-l-lg cursor-pointer transition-colors" title="Logout" tabIndex={0} onClick={() => quitRoom(send, user)} onKeyPress={(event) => { if (event.code === 'Enter') quitRoom(send, user); }}>
+      <LogoutIcon />
+    </div>
     <Id id={room.id} copy />
   </div>
 );
