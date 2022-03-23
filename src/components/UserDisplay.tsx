8bc4c5a9b2abf46fc59df68a943528dff0b337c7
Vazanoir <vazanoir@outlook.fr>
2022-03-23 06:39:03 +0100

    Only show 5 numbers of user ids

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -1,18 +1,19 @@
 import { PencilAltIcon } from '@heroicons/react/solid';
 import React from 'react';
+import { idLength } from '../constants';
 import { ModalType, User } from '../utils/types';
 import Id from './Id';
 
 type UserDisplayProps = { user: User, modal: ModalType };
 
 const UserDisplay = ({ user, modal }: UserDisplayProps) => (
-  <div>
+  <div className="flex justify-between">
     <button className="text-md font-medium flex items-center" onClick={() => modal.toggle()} type="button">
       {user.name}
       {' '}
       <PencilAltIcon className="ml-2 h-5 w-4" />
     </button>
-    <Id id={user.id} />
+    <Id id={user.id.substring(0, idLength)} />
   </div>
 );
 
