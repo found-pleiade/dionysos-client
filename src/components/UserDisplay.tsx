217b66f68a52da372786627db6eb358a04315d54
Vazanoir <vazanoir@outlook.fr>
2022-03-23 20:34:37 +0100

    Add title to UserDisplay

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -6,13 +6,11 @@ import Id from './Id';
 type UserDisplayProps = { user: User, modal: ModalType };
 
 const UserDisplay = ({ user, modal }: UserDisplayProps) => (
-  <div className="flex justify-between">
-    <button className="text-md font-medium flex items-center" onClick={() => modal.toggle()} type="button">
-      {user.name}
-      <Id id={user.id} short inline className="px-1 cursor-pointer" />
-      <PencilAltIcon className="h-5 w-4" />
-    </button>
-  </div>
+  <button className="text-md font-medium flex items-center" onClick={() => modal.toggle()} type="button" title="Click to change your username">
+    {user.name}
+    <Id id={user.id} short inline className="px-1 cursor-pointer" />
+    <PencilAltIcon className="h-5 w-4" />
+  </button>
 );
 
 export default UserDisplay;
