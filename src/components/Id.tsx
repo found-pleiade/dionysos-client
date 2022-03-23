bef9205e6eb2e6e0aa83dc7a9887bbc2a9cefdee
Vazanoir <vazanoir@outlook.fr>
2022-03-23 06:38:46 +0100

    Bigger id font size

--- src/components/Id.tsx
+++ src/components/Id.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ClipboardIcon } from '@heroicons/react/solid';
 
 const Id = ({ id }: { id: string }) => (
-  <button className="text-sm text-foreground/40 cursor-pointer hover:text-foreground/60 flex transition-colors" title="Click to copy" onClick={() => navigator.clipboard.writeText(id)} type="button">
+  <button className="text-md text-foreground/40 cursor-pointer hover:text-foreground/60 flex transition-colors" title="Click to copy" onClick={() => navigator.clipboard.writeText(id)} type="button">
     {id}
     {' '}
     <ClipboardIcon className="ml-2 w-5 h-5 items-center" />
