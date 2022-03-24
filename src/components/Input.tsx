aaf7205ba189225417b59197c1bee5b1d51aae38
Vazanoir <vazanoir@outlook.fr>
2022-03-24 11:54:36 +0100

    Replace generic tag with class

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -36,7 +36,7 @@ const Input = ({
         onKeyPress={onKeyPress}
         placeholder={' '}
       />
-      <p className="absolute top-2 left-3 font-medium cursor-text text-background-400/70 unclickable transition-all">{placeholder}</p>
+      <p className="absolute top-2 left-3 font-medium cursor-text text-background-400/70 unclickable transition-all placeholder">{placeholder}</p>
 
       <div className={`absolute z-10 bg-foreground text-background-900 w-full rounded-b-lg px-3 py-2 flex flex-col space-y-1 border-t-2 mt-1 font-medium text-sm ${visibility(showHelper)}`}>
         <p className={`text-error ${visibility(!gteThree)}`}>At least 3 characters</p>
