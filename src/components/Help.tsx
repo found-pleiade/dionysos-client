7ff04e390f60f8054cecb2597c9446346f20eb98
Vazanoir <vazanoir@outlook.fr>
2022-03-26 07:20:50 +0100

    Fix help typos

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -5,9 +5,9 @@ const Help = ({ shown }: { shown: boolean }) => (
   <div className={`z-10 absolute right-0 top-3 font-medium bg-background-600 text-foreground p-3 rounded-r-lg w-[44ch] whitespace-normal translate-x-[100%] ${visibility(shown)}`}>
     <h1 className="font-black text-xl">Basics</h1>
     <p className="font-bold">
-      The person wanting to share the film need to create the room
+      The person owning the film need to create the room
       which will create an ID under the room name.
-      This ID the what you give for others to join the room.
+      This ID is what you share for others to join the room.
     </p>
     <br />
     <h1 className="font-bold text-xl text-foreground/90">Create a room</h1>
