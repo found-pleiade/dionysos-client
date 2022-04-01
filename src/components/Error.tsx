8fef4b400407188aab9810022b9e4107fbe1945f
Vazanoir <vazanoir@outlook.fr>
2022-04-01 23:18:30 +0200

    Add custom duration support and clear function

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import * as R from 'ramda';
+import { v4 } from 'uuid';
+import { notNil } from '../utils';
 
 const Error = ({
-  error,
-}: { error: string }) => (
-  <p className="first-letter:uppercase bg-error font-medium p-2 last:rounded-b-md error w-full text-center">
-    {error}
-  </p>
-);
+  error, duration,
+}: { error: string | undefined, duration?: number }) => {
+  if (R.isNil(error)) return <div />;
+
+  const animationDuration = notNil(duration) ? duration : (error.length / 5);
+
+  return (
+    <p key={v4()} className="first-letter:uppercase bg-error font-medium p-2 last:rounded-b-md error w-full text-center" style={{ animationDuration: `${animationDuration}s` }}>
+      {error}
+    </p>
+  );
+};
 
 export default Error;
