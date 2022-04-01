cb5d436bdf2504d469db23e9704e4688883a1485
Vazanoir <vazanoir@outlook.fr>
2022-04-02 00:13:58 +0200

    Replace render with createRoot

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOMClient.createRoot(
+  document.getElementById('root') as Element | DocumentFragment,
+);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
