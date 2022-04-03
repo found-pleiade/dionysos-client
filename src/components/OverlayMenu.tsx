4e018392020e8d62a563f42c9e6f6a440f12b5d6
Vazanoir <vazanoir@outlook.fr>
2022-04-03 23:51:05 +0200

    Fix side menu not opening

--- src/components/OverlayMenu.tsx
+++ src/components/OverlayMenu.tsx
@@ -12,13 +12,13 @@ const OverlayMenu = ({
   panel, chat,
 }: OverlayMenuProps) => {
   const iconStyle = 'block p-2 h-8 w-8 bg-background-600 rounded-full cursor-pointer text-center opacity-60 hover:opacity-100 transition-opacity';
-  const iconMarge = !panel && !chat ? 'mb-3 last:mb-0' : '';
+  const iconMarge = !panel.isOpen && !chat.isOpen ? 'mb-3 last:mb-0' : '';
 
   return (
     <div className="absolute top-[50%] left-3 z-10 translate-y-[-50%]">
-      <MenuIcon className={`${iconStyle} ${iconMarge} ${visibility(!panel)}`} onClick={() => panel.toggle} />
+      <MenuIcon className={`${iconStyle} ${iconMarge} ${visibility(!panel.isOpen)}`} onClick={() => panel.toggle()} />
 
-      <ChatAltIcon className={`${iconStyle} ${iconMarge} ${visibility(!chat)}`} onClick={() => chat.toggle()} />
+      <ChatAltIcon className={`${iconStyle} ${iconMarge} ${visibility(!chat.isOpen)}`} onClick={() => chat.toggle()} />
     </div>
   );
 };
