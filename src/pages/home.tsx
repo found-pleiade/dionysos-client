694fa558470cb99a9175b109d5b0da4127dde7bb
Vazanoir <vazanoir@outlook.fr>
2022-04-05 22:51:47 +0200

    Remove unused videojs code and move types

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import Videojs from '../components/Videojs';
 import { JoinRequest } from '../utils/types';
 import OverlayMenu from '../components/OverlayMenu';
@@ -18,15 +18,6 @@ import UserDisplay from '../components/UserDisplay';
 import ChangeNameModal from '../components/ChangeNameModal';
 import Chat from '../components/Chat';
 
-type homeProps = {
-  connection: ReturnType<typeof useConnection>,
-  users: ReturnType<typeof useUsers>,
-  room: ReturnType<typeof useRoom>,
-  joinRequestModal: ReturnType<typeof useModal>,
-  joinRequests: Array<JoinRequest>,
-  setJoinRequests: React.Dispatch<React.SetStateAction<Array<JoinRequest>>>,
-}
-
 const Home = ({
   connection,
   users,
@@ -34,34 +25,14 @@ const Home = ({
   joinRequestModal,
   joinRequests,
   setJoinRequests,
-}: homeProps) => {
-  const playerRef = useRef(null);
-
-  const videoJsOptions = {
-    autoplay: false,
-    controls: true,
-    fullscreen: true,
-    fluid: true,
-    techOrder: ['youtube'],
-    sources: [{
-      src: 'https://www.youtube.com/watch?v=_VjPDGdUIDI',
-      type: 'video/youtube',
-    }],
-  };
-
-  const handlePlayerReady = (player: any) => {
-    playerRef.current = player;
-
-    // you can handle player events here
-    player.on('waiting', () => {
-      // console.log('player is waiting');
-    });
-
-    player.on('dispose', () => {
-      // console.log('player will dispose');
-    });
-  };
-
+}: {
+  connection: ReturnType<typeof useConnection>,
+  users: ReturnType<typeof useUsers>,
+  room: ReturnType<typeof useRoom>,
+  joinRequestModal: ReturnType<typeof useModal>,
+  joinRequests: Array<JoinRequest>,
+  setJoinRequests: React.Dispatch<React.SetStateAction<Array<JoinRequest>>>,
+}) => {
   /**
    * State of the visibility of the panel, help, chat and modal.
    */
@@ -124,8 +95,8 @@ const Home = ({
           {/* Player */}
           <div className="video-max-width mx-auto w-[inherit]">
             <Videojs
-              options={videoJsOptions}
-              onReady={handlePlayerReady}
+              options={{}}
+              onReady={() => { }}
             />
           </div>
 
