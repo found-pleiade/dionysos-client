39f9a99adf7e4cc6e8b8c6656f03d38b8860c2e5
Vazanoir <vazanoir@outlook.fr>
2022-04-11 04:04:36 +0200

    Add shadows and transparency to side menues

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -10,7 +10,7 @@ const Chat = ({
   state: ReturnType<typeof useSideMenu>,
   children: React.ReactNode
 }) => (
-  <div className={`flex flex-col justify-between dark:bg-dark-dominant-700 relative transition-all py-3 ${translate(state.isOpen)}`}>
+  <div className={`flex flex-col justify-between dark:bg-dark-dominant-700/60 relative transition-all py-3 ${translate(state.isOpen)} shadow-xl shadow-dark-dominant-800`}>
     <MinimizeIcon toggle={state.toggle} />
     {children}
   </div>
--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -10,7 +10,7 @@ const Panel = ({
   state: ReturnType<typeof useSideMenu>,
   children: React.ReactNode,
 }) => (
-  <div className={`flex flex-col justify-between dark:bg-dark-dominant-800 relative transition-all py-3 ${translate(state.isOpen)}`}>
+  <div className={`flex flex-col justify-between dark:bg-dark-dominant-800/60 relative transition-all py-3 ${translate(state.isOpen)} shadow-xl shadow-dark-dominant-800 z-[1]`}>
     <MinimizeIcon toggle={state.toggle} />
     {children}
   </div>
