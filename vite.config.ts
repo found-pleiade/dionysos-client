8f626e31752e33f59d9f94419e109c16647bad11
Vazanoir <vazanoir@outlook.fr>
2022-06-15 00:13:51 +0200

    Add more config for tests

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {},
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 });
