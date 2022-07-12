831fa0b53ee2b95b7c36993abf8be8e6fd7b47bf
Vazanoir <vazanoir@outlook.fr>
2022-07-12 03:13:40 +0200

    Add a hook for settings

---
+++ src/hooks/settings.ts
@@ -0,0 +1,47 @@
+import { useReducer } from 'react';
+
+const useSettings = () => {
+  const initialSettings = {
+    server: 'http://localhost:3000',
+  };
+
+  type Settings = typeof initialSettings;
+
+  enum SettingsActionList {
+    SET_SERVER = 'SET_SERVER',
+    SET_SERVER_DEFAULT = 'SET_SERVER_DEFAULT',
+  }
+
+  type SettingsAction = {
+    type: keyof typeof SettingsActionList;
+    payload: {
+      server?: typeof initialSettings.server;
+    };
+  }
+
+  const settingsReducer = (
+    state: Settings,
+    action: SettingsAction,
+  ) => {
+    switch (action.type) {
+      case SettingsActionList.SET_SERVER:
+        return {
+          ...state,
+          ...action.payload,
+        };
+      case SettingsActionList.SET_SERVER_DEFAULT:
+        return {
+          ...state,
+          server: initialSettings.server,
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [get, dispatch] = useReducer(settingsReducer, initialSettings);
+
+  return { get, dispatch };
+};
+
+export default useSettings;
