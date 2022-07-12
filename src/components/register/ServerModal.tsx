4fb4b6846809e18e1cc50fdc8d71db9d9d79e832
Vazanoir <vazanoir@outlook.fr>
2022-07-12 04:21:55 +0200

    Fix light background for ServerModal

--- src/components/register/ServerModal.tsx
+++ src/components/register/ServerModal.tsx
@@ -90,7 +90,7 @@ const ServerModal = () => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all dark:bg-dark-primary-700 dark:text-dark-secondary">
+                <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl p-6 text-left align-middle shadow-xl transition-all bg-light-primary-100  dark:bg-dark-primary-700 dark:text-dark-secondary">
                   <Dialog.Title
                     as="h3"
                     className="text-lg font-medium leading-6 mb-4"
