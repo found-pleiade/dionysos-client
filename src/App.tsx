d9a0dc1c938cdac35dfb7c74da88e85ee6162d40
Vazanoir <vazanoir@outlook.fr>
2022-07-17 00:19:09 +0200

    Create and use a user context

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import Register from './pages/register';
 import Home from './pages/home';
 import SettingsContext from './contexts/SettingContext';
 import useSettings from './hooks/settings';
+import useUser from './hooks/user';
+import UserContext from './contexts/UserContext';
 
 const App = () => {
   const queryClient = new QueryClient({
@@ -18,29 +20,32 @@ const App = () => {
   });
 
   const settings = useSettings();
+  const user = useUser();
 
   return (
     <SettingsContext.Provider value={settings}>
-      <QueryClientProvider client={queryClient}>
-        <div className="dark:text-dark-secondary h-screen cursor-default relative">
-          <MemoryRouter>
-            <Routes>
-              <Route
-                path="/"
-                element={(
-                  <Register />
-                )}
-              />
-              <Route
-                path="/home"
-                element={(
-                  <Home />
-                )}
-              />
-            </Routes>
-          </MemoryRouter>
-        </div>
-      </QueryClientProvider>
+      <UserContext.Provider value={user}>
+        <QueryClientProvider client={queryClient}>
+          <div className="dark:text-dark-secondary h-screen cursor-default relative">
+            <MemoryRouter>
+              <Routes>
+                <Route
+                  path="/"
+                  element={(
+                    <Register />
+                  )}
+                />
+                <Route
+                  path="/home"
+                  element={(
+                    <Home />
+                  )}
+                />
+              </Routes>
+            </MemoryRouter>
+          </div>
+        </QueryClientProvider>
+      </UserContext.Provider>
     </SettingsContext.Provider>
   );
 };
