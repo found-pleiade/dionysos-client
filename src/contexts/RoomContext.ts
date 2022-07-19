4a0026dcd876300402186fe0fb03f81ec73263a2
Vazanoir <vazanoir@outlook.fr>
2022-07-19 20:54:18 +0200

    Add room context

---
+++ src/contexts/RoomContext.ts
@@ -0,0 +1,6 @@
+import { Context, createContext } from 'react';
+import useUser from '../hooks/user';
+
+const UserContext = createContext(null as any);
+
+export default UserContext as Context<ReturnType<typeof useUser>>;
