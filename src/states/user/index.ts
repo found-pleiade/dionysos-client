d2d8728d529f83585aa0f635da60ee3fb7379832
Vazanoir <vazanoir@outlook.fr>
2022-07-29 20:08:09 +0200

    Use union to ensure type based on action type

--- src/states/user/index.ts
+++ src/states/user/index.ts
@@ -1,5 +1,4 @@
-import * as R from 'ramda';
-import { useReducer, createContext } from 'react';
+import { useReducer, createContext } from "react";
 
 const useUser = () => {
   const baseUser = {
@@ -8,27 +7,31 @@ const useUser = () => {
     name: "",
   };
 
-  enum UserActionList {
-    SET_URI_AND_ID = "SET_URI_AND_ID",
-    SET_NAME = "SET_NAME",
-  }
+  const actionTypes = Object.freeze({
+    SET_URI_AND_ID: "SET_URI_AND_ID",
+    SET_NAME: "SET_NAME",
+  }); 
 
-  const UserError = (err: string) => new Error(`useUser: ${err}`);
+  // Specify the type of the payload based on the type
+  type Action =
+    | {
+        type: typeof actionTypes["SET_URI_AND_ID"];
+        payload: {
+          uri: typeof baseUser.uri;
+        };
+      }
+    | {
+        type: typeof actionTypes["SET_NAME"];
+        payload: {
+          name: typeof baseUser.name;
+        };
+      };
 
-  const userReducer = (
-    state: typeof baseUser,
-    action: {
-      type: keyof typeof UserActionList;
-      payload: Partial<typeof baseUser>;
-    }
-  ) => {
+  const reducer = (state: typeof baseUser, action: Action) => {
     switch (action.type) {
-      case UserActionList.SET_URI_AND_ID: {
+      case actionTypes["SET_URI_AND_ID"]: {
         const { uri } = action.payload;
-        if (!uri) throw UserError("missing uri");
-
         const id = Number(uri.split("/").pop());
-        if (!id) throw UserError("missing id in uri");
 
         return {
           ...state,
@@ -36,21 +39,18 @@ const useUser = () => {
           id,
         };
       }
-      case UserActionList.SET_NAME: {
+      case actionTypes["SET_NAME"]: {
         const { name } = action.payload;
-        if (R.isNil(name)) throw UserError("missing name");
 
         return {
           ...state,
           name,
         };
       }
-      default:
-        throw UserError(`unhandled action type: ${action.type}`);
     }
   };
 
-  const [get, dispatch] = useReducer(userReducer, baseUser);
+  const [get, dispatch] = useReducer(reducer, baseUser);
 
   return { get, dispatch };
 };
