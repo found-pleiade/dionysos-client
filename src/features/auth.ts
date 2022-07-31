611cb24d66e989cae4bc1db884d51005691cfeeb
Vazanoir <vazanoir@outlook.fr>
2022-07-31 23:49:24 +0200

    Add comments to auth

--- src/features/auth.ts
+++ src/features/auth.ts
@@ -1,9 +1,13 @@
 import { createContext, useState } from "react";
 import useUser from "../states/user";
 
+// Basic auth where you get a password on user
+// creation that you add in the headers of
+// requests.
 const useAuth = () => {
   const [password, setPassword] = useState("");
-
+  
+  // Headers abstraction that handle auth.
   const newHeaders = (
     user: Pick<ReturnType<typeof useUser>, "get">,
     headers = {} as Headers
