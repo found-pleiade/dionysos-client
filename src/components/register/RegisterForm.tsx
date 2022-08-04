199b5507ce5fb5f5e8cbd717d1eeb9c202c2af2a
Vazanoir <vazanoir@outlook.fr>
2022-08-04 14:36:19 +0200

    22 add an input helper (#112)
    * Add helper to the last input
    
    * Remove unused import
    
    * Reduce the gap

--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -5,8 +5,9 @@ import useCreateUser from "../../states/user/createUser";
 import Button from "../Button";
 import Input from "../Input";
 import RowGroup from "../../layouts/RowGroup";
-import { isValid } from "../../utils";
+import { isValid, isValidConditions } from "../../utils";
 import { AuthContext } from "../../features/auth";
+import ErrorCard from "../ErrorCard";
 
 const RegisterForm = () => {
   const [name, setName] = useState("");
@@ -44,24 +45,30 @@ const RegisterForm = () => {
   };
 
   return (
-    <RowGroup>
-      <Input
-        id="connect"
-        className="rounded-r-none bg-light-primary-100 focus:bg-light-primary-100"
-        placeholder="Username"
-        value={name}
-        setValue={setName}
-      />
+    <div className="flex flex-col w-full gap-1">
+      <RowGroup>
+        <Input
+          id="connect"
+          className="rounded-r-none bg-light-primary-100 focus:bg-light-primary-100"
+          placeholder="Username"
+          value={name}
+          setValue={setName}
+        />
 
-      <Button
-        className="rounded-l-none"
-        onClick={mutate}
-        loading={isLoading}
-        disabled={!isValid(name)}
-      >
-        {buttonText()}
-      </Button>
-    </RowGroup>
+        <Button
+          className="rounded-l-none"
+          onClick={mutate}
+          loading={isLoading}
+          disabled={!isValid(name)}
+        >
+          {buttonText()}
+        </Button>
+      </RowGroup>
+
+      <ErrorCard show={!isValidConditions.lteTwenty(name)}>
+        Maximum length is 20 chars
+      </ErrorCard>
+    </div>
   );
 };
 
