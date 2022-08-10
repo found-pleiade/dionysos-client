a6687a3a980774ddd73a79a72d62f75ff8b7152e
Vazanoir <vazanoir@outlook.fr>
2022-08-11 00:20:24 +0200

    Remove classes about dialog width

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -91,7 +91,7 @@ const Home = () => {
                       leaveFrom="opacity-100 scale-100"
                       leaveTo="opacity-0 scale-95"
                     >
-                      <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl p-6 text-center align-middle shadow-xl transition-all bg-light-primary-100  dark:bg-dark-primary-700 dark:text-dark-secondary">
+                      <Dialog.Panel className="transform overflow-hidden rounded-2xl p-6 text-center align-middle shadow-xl transition-all bg-light-primary-100  dark:bg-dark-primary-700 dark:text-dark-secondary">
                         <p className="text-xl font-bold">
                           Share the following link to your friends:
                         </p>
