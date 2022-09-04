99cf2ef850d8aee07479b7c117f185b69d75eaf9
Vazanoir <vazanoir@outlook.fr>
2022-09-04 13:44:57 +0200

    Add getRoom hook

---
+++ src/states/room/getRoom.ts
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { useQuery } from "react-query";
+import { SettingsContext } from "../settings";
+import { UserContext } from "../user";
+import { AuthContext } from "../../features/auth";
+import { ShareContext } from "../../features/shareRoom";
+
+const useGetRoom = () => {
+  const settings = useContext(SettingsContext);
+  const user = useContext(UserContext);
+  const auth = useContext(AuthContext);
+  const share = useContext(ShareContext);
+
+  const { isLoading, error, data, refetch } = useQuery(
+    "getRoom",
+    () => {
+      fetch(`${settings.get.server}/rooms/${share.id}`, {
+        headers: auth.newHeaders(user),
+      }).then((res) => res.json());
+    },
+    {
+      enabled: false,
+    }
+  );
+
+  return {
+    isLoading,
+    error,
+    data,
+    refetch,
+  };
+};
+
+export default useGetRoom;
