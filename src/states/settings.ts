e912ea42248da282eb4f04be38de42fd078281cc
Vazanoir <vazanoir@outlook.fr>
2022-09-08 17:46:57 +0200

    Fix server url

--- src/states/settings.ts
+++ src/states/settings.ts
@@ -7,7 +7,7 @@ enum ActionTypes {
 
 const useSettings = () => {
   const initialSettings = {
-    server: "https://dionysos-api-pr-112.onrender.com",
+    server: "https://ipa.dionysos.live",
   };
 
   // Specify the type of the payload based on the type
