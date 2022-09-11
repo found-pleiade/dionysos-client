e90e0ca9110f0dfe9e1c53438375671701467092
Vazanoir <vazanoir@outlook.fr>
2022-09-11 23:39:57 +0200

    Refactor register page with some mobile style

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -14,58 +14,61 @@ const Register = () => {
     share.scanUrl();
   }, []);
 
-  const pageSkeleton = (template: JSX.Element) => (
+  const pageState = () => {
+    if (isLoading) {
+      return (
+        <CenterCard>
+          <p className="font-medium text-xl text-center">
+            Connection to the server
+          </p>
+
+          <LinearLoader />
+        </CenterCard>
+      );
+    }
+
+    if (error ? true : false) {
+      return (
+        <CenterCard>
+          Connection to the server failed.
+          <br />
+          Check your internet connection or try again later.
+        </CenterCard>
+      );
+    }
+
+    if (!isCompatible && isCorrect) {
+      return (
+        <CenterCard>
+          Version mismatch between the client and the server.
+        </CenterCard>
+      );
+    }
+
+    if (!isCompatible && !isCorrect) {
+      return <CenterCard>The server url seems wrong.</CenterCard>;
+    }
+
+    return <RegisterForm />;
+  };
+
+  return (
     <div className="page">
       {/* Main content of the page. */}
-      <div className="absolute top-[43%] left-[50%] min-h-[500px] translate-x-[-50%] translate-y-[-50%] text-center flex flex-col items-center">
-        <header className="xl:mb-52 md:mb-32 mb-20">
-          <h1 className="text-[6rem] -mb-6 font-black uppercase">Dyonisos</h1>
-          <h2 className="text-[2rem] font-semibold">
+      <div className="h-screen text-center pt-8">
+        <header className="mb-10">
+          <h1 className="text-[3rem] font-black uppercase">Dyonisos</h1>
+          <h2 className="text-[1.1rem] -mt-3 font-semibold">
             Share cinematic experiences.
           </h2>
         </header>
 
-        {template}
+        {pageState()}
       </div>
 
       <ServerModal />
     </div>
   );
-
-  if (isLoading) {
-    return pageSkeleton(
-      <CenterCard>
-        <p className="font-medium text-xl text-center">
-          Connection to the server
-        </p>
-        <LinearLoader />
-      </CenterCard>
-    );
-  }
-
-  if (error) {
-    return pageSkeleton(
-      <CenterCard>
-        Connection to the server failed.
-        <br />
-        Check your internet connection or try again later.
-      </CenterCard>
-    );
-  }
-
-  if (!isCompatible && isCorrect) {
-    return pageSkeleton(
-      <CenterCard>
-        Version mismatch between the client and the server.
-      </CenterCard>
-    );
-  }
-
-  if (!isCompatible && !isCorrect) {
-    return pageSkeleton(<CenterCard>The server url seems wrong.</CenterCard>);
-  }
-
-  return pageSkeleton(<RegisterForm />);
 };
 
 export default Register;
