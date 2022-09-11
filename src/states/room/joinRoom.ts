9a8628cb1db6f4ece12f5dd8a6fa1fceda27d674
Vazanoir <vazanoir@outlook.fr>
2022-09-11 15:17:08 +0200

    Fix joinRoom return value and type

--- src/states/room/joinRoom.ts
+++ src/states/room/joinRoom.ts
@@ -10,14 +10,14 @@ const useJoinRoom = (shareId: string) => {
   const user = useContext(UserContext);
   const auth = useContext(AuthContext);
 
-  const { isLoading, error, isSuccess, refetch } = useQuery<undefined, Error>(
+  const { isLoading, error, isSuccess, refetch } = useQuery<Response, Error>(
     ["joinRoom"],
     () =>
       fetch(`${settings.get.server}/rooms/${shareId}/connect`, {
         headers: auth.newHeaders(user),
         method: "PATCH",
       }).then((res) => {
-        if (isRequestValid(res)) return res.json();
+        if (isRequestValid(res)) return res;
         throw new Error("Something went wrong");
       }),
     {
