57a857dfd4126398bf946f28539e22dda0b317b7
Vazanoir <vazanoir@outlook.fr>
2022-09-12 04:04:20 +0200

    Set new colors and fix css

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import "@fontsource/source-sans-pro";
 import "@fontsource/montserrat";
+import "@fontsource/montserrat/500.css";
+import "@fontsource/montserrat/800.css";
 
 const root = ReactDOMClient.createRoot(
   document.getElementById("root") as Element | DocumentFragment
