be928e6d30657a7c485448812ad0c2dc4aef1694
Vazanoir <vazanoir@outlook.fr>
2022-09-16 17:12:43 +0200

    Readd fixed test

--- src/components/Button.test.tsx
+++ src/components/Button.test.tsx
@@ -12,117 +12,43 @@ describe("Renders", () => {
     expect(screen.getByRole("none").tagName).toBe("DIV");
   });
 
-  it.skip("basic", () => {
+  it("basic", () => {
     render(<Button>Hello World</Button>);
 
     expect(screen.getByText("Hello World").tagName).toBe("BUTTON");
     expect(
-      screen.getByRole("button").classList.contains("bg-light-accent-500")
+      screen.getByRole("button").classList.contains("text-light-accent-400")
     ).toBe(true);
   });
 
-  it.skip("disabled", () => {
+  it("disabled", () => {
     render(<Button disabled>Hello World</Button>);
 
     expect(screen.getByRole("button").hasAttribute("disabled")).toBe(true);
     expect(
-      screen.getByRole("button").classList.contains("bg-light-accent-500")
-    ).toBe(true);
-    expect(
       screen.getByRole("button").classList.contains("cursor-not-allowed")
     ).toBe(true);
   });
 
-  it.skip("colorless", () => {
+  it("colorless", () => {
     render(<Button colorless>Hello World</Button>);
 
     expect(
-      screen.getByRole("button").classList.contains("bg-light-primary-300")
+      screen.getByRole("button").classList.contains("!text-light-secondary-900")
     ).toBe(true);
-  });
-
-  it.skip("colorless and disabled", () => {
-    render(
-      <Button colorless disabled>
-        Hello World
-      </Button>
-    );
-
-    expect(screen.getByRole("button").hasAttribute("disabled")).toBe(true);
-    expect(
-      screen.getByRole("button").classList.contains("cursor-not-allowed")
-    ).toBe(true);
-    expect(
-      screen.getByRole("button").classList.contains("bg-light-primary-300")
-    ).toBe(true);
-  });
-});
-
-describe("Router link", () => {
-  it.skip("button", () => {
-    render(<Button>Hello World</Button>, { wrapper: MemoryRouter });
-
-    expect(screen.getByText("Hello World").tagName).toBe("BUTTON");
-  });
-
-  it.skip("link", () => {
-    render(<Button to="test">Hello World</Button>, { wrapper: MemoryRouter });
-
-    expect(screen.getByText("Hello World").tagName).toBe("A");
-  });
-
-  it.skip("disabled link", () => {
-    render(
-      <Button disabled to="test">
-        Hello World
-      </Button>,
-      { wrapper: MemoryRouter }
-    );
-
-    expect(screen.getByText("Hello World").tagName).toBe("BUTTON");
-    expect(screen.getByText("Hello World").hasAttribute("disabled")).toBe(true);
-  });
-
-  it.skip("empty link", () => {
-    render(<Button to="">Hello World</Button>, { wrapper: MemoryRouter });
-
-    expect(screen.getByText("Hello World").tagName).toBe("BUTTON");
   });
 });
 
 describe("Events", () => {
-  describe("Button", () => {
-    it("click", () => {
-      let clicked = false;
-      const func = () => {
-        clicked = true;
-      };
+  it("click", () => {
+    let clicked = false;
+    const onClick = () => {
+      clicked = true;
+    };
 
-      render(<Button onClick={func}>Hello World</Button>, {
-        wrapper: MemoryRouter,
-      });
+    render(<Button onClick={onClick}>Hello World</Button>);
 
-      screen.getByText("Hello World").click();
-      expect(clicked).toBe(true);
-    });
-  });
-
-  describe("Router link", () => {
-    it("click", () => {
-      let clicked = false;
-      const func = () => {
-        clicked = true;
-      };
-
-      render(
-        <Button to="test" onClick={func}>
-          Hello World
-        </Button>,
-        { wrapper: MemoryRouter }
-      );
-
-      screen.getByText("Hello World").click();
-      expect(clicked).toBe(true);
-    });
+    screen.getByText("Hello World").click();
+    expect(clicked).toBe(true);
   });
 });
