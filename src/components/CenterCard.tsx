4e4f212a144840c838265a2ac66732c81a393c40
Vazanoir <vazanoir@outlook.fr>
2022-09-16 03:22:03 +0200

    Fix CenterCard colors

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 
 const CenterCard = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex flex-col items-center space-y-3 bg-light-primary-100 dark:bg-dark-primary-700 p-10 rounded-none md:rounded-md max-w-3xl">
-    <div className="font-semibold text-xl text-center text-light-secondary dark:text-dark-secondary">
-      {children}
-    </div>
+  <div className="flex flex-col items-center space-y-3 p-10 rounded-none md:rounded-md max-w-3xl">
+    <div className="font-semibold text-xl text-center">{children}</div>
   </div>
 );
 
