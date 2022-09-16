eecdf817ad4d446900f1e71acd1d8d1a602fb10b
Vazanoir <vazanoir@outlook.fr>
2022-09-16 03:24:22 +0200

    Remove useless template and css

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 
 const CenterCard = ({ children }: { children: React.ReactNode }) => (
-  <div className="flex flex-col items-center space-y-3 p-10 rounded-none md:rounded-md max-w-3xl">
-    <div className="font-semibold text-xl text-center">{children}</div>
-  </div>
+  <div className="max-w-3xl font-semibold text-xl text-center">{children}</div>
 );
 
 export default CenterCard;
