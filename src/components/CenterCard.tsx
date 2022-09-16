7cf73bf8ff2476da9bb91886678731160cdc3c97
Vazanoir <vazanoir@outlook.fr>
2022-09-16 05:14:52 +0200

    Style CenterCard back

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const CenterCard = ({ children }: { children: React.ReactNode }) => (
-  <div className="max-w-3xl font-semibold text-xl text-center">{children}</div>
+  <div
+    className="w-full max-w-3xl font-medium text-lg text-center
+  bg-light-primary-200 dark:bg-dark-primary-800 p-4 sm:rounded-md"
+  >
+    {children}
+  </div>
 );
 
 export default CenterCard;
