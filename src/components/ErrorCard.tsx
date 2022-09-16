d640ea5b075d9205addfbce7ef48b939b431657d
Vazanoir <vazanoir@outlook.fr>
2022-09-16 05:15:49 +0200

    Edit ErrorCard

--- src/components/ErrorCard.tsx
+++ src/components/ErrorCard.tsx
@@ -15,13 +15,13 @@ const ErrorCard = ({
     as={Fragment}
     enter="ease-out duration-300"
     enterFrom="max-h-0 py-0"
-    enterTo="max-h-24 py-2"
+    enterTo="max-h-32 py-2"
     leave={"ease-in duration-200"}
-    leaveFrom="max-h-24 py-2"
+    leaveFrom="max-h-32 py-2"
     leaveTo="max-h-0 py-0"
   >
     <div
-      className={`rounded-md overflow-hidden px-4 py-2 mb-4 font-semibold text-light-primary-100 bg-light-error-400 dark:text-dark-error-100 dark:bg-dark-error-900 select-text ${className}`}
+      className={`rounded-md overflow-hidden px-4 py-2 mb-4 font-medium text-light-primary-100 bg-light-error-400 dark:text-dark-error-100 dark:bg-dark-error-900 select-text ${className}`}
     >
       {children}
     </div>
