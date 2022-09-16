6123a508fbe6baee67ff01a63cd0e281e487d708
Vazanoir <vazanoir@outlook.fr>
2022-09-16 06:08:25 +0200

    Fix join create ui and add h-full to home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -81,10 +81,12 @@ const Home = () => {
   if (joinRoom.isLoading && createRoom.isLoading) {
     return (
       <PlaceItemsCenter fullscreen>
-        <p className="text-xl text-center">
-          {share.isJoining ? "Joining the room" : "Creating the room"}
-        </p>
-        <LinearLoader />
+        <div className="text-center flex flex-col items-center">
+          <p className="text-xl text-center">
+            {share.isJoining ? "Joining the room" : "Creating the room"}
+          </p>
+          <LinearLoader />
+        </div>
       </PlaceItemsCenter>
     );
   }
@@ -92,126 +94,126 @@ const Home = () => {
   if (createRoom.error || joinRoom.error) {
     return (
       <PlaceItemsCenter fullscreen>
-        {createRoom.error?.message}
-        {joinRoom.error?.message}
-        <br />
-        Please refresh the page and try again
+        <div className="text-center">
+          {createRoom.error?.message}
+          {joinRoom.error?.message}
+          <br />
+          Please refresh the page and try again
+        </div>
       </PlaceItemsCenter>
     );
   }
 
   return (
-    <div className="page">
-      <div
-        id="background"
-        className="h-dvh w-screen truncate flex"
-        onClick={(e) => {
-          const target = e.target as HTMLDivElement;
-          if (target.id === "background" || target.id === "video") {
-            return panel.setIsOpen(false);
-          }
-        }}
-      >
-        <Panel state={panel}>
-          <div className="flex ml-auto">
-            <Button
-              className="min-w-0 w-10 h-10 px-2.5 rounded-full"
-              onClick={() => {
-                setIsDialogOpen(true);
-              }}
-            >
-              <ShareIcon />
-            </Button>
+    <div
+      id="background"
+      className="h-full w-screen truncate flex"
+      onClick={(e) => {
+        const target = e.target as HTMLDivElement;
+        if (target.id === "background" || target.id === "video") {
+          return panel.setIsOpen(false);
+        }
+      }}
+    >
+      <Panel state={panel}>
+        <div className="flex ml-auto">
+          <Button
+            className="min-w-0 w-10 h-10 px-2.5 rounded-full"
+            onClick={() => {
+              setIsDialogOpen(true);
+            }}
+          >
+            <ShareIcon />
+          </Button>
 
+          <Button
+            className="min-w-0 w-10 h-10 px-2 rounded-full"
+            onClick={panel.toggle}
+            colorless
+          >
+            <XIcon />
+          </Button>
+        </div>
+
+        <SimpleDialog
+          show={isDialogOpen}
+          closeFunction={() => {
+            panel.setIsOpen(true);
+            setIsDialogOpen(false);
+          }}
+          className="text-center max-w-3xl w-auto"
+        >
+          <p className="text-lg font-semibold">
+            Share this link with your friends
+          </p>
+
+          <Button
+            headless
+            className="font-mono my-3"
+            onClick={() => {
+              navigator.clipboard.writeText(url);
+              setUrlCopied(true);
+
+              setTimeout(() => {
+                setUrlCopied(false);
+              }, 3000);
+            }}
+          >
+            <p>{url}</p>
+
+            <p
+              className={`${
+                urlCopied
+                  ? "text-light-accent-400 dark:text-dark-accent-400"
+                  : "text-light-secondary-500 dark:text-dark-secondary-500"
+              }`}
+            >
+              {urlCopied ? "copied!" : "click to copy"}
+            </p>
+          </Button>
+
+          <SpaceBetween>
+            <div />
             <Button
-              className="min-w-0 w-10 h-10 px-2 rounded-full"
-              onClick={panel.toggle}
+              onClick={() => {
+                panel.setIsOpen(true);
+                setIsDialogOpen(false);
+              }}
               colorless
             >
-              <XIcon />
+              Done
             </Button>
-          </div>
+          </SpaceBetween>
+        </SimpleDialog>
 
-          <SimpleDialog
-            show={isDialogOpen}
-            closeFunction={() => {
-              panel.setIsOpen(true);
-              setIsDialogOpen(false);
-            }}
-            className="text-center max-w-3xl w-auto"
-          >
-            <p className="text-lg font-semibold">
-              Share this link with your friends
-            </p>
+        <ul className="h-full">
+          {getRoom.data?.users.map((user: any) => {
+            return (
+              <li key={user.ID} className="pb-1 flex align-middle">
+                {user.name}
+                {user.ID === getRoom.data?.ownerID ? (
+                  <StarIcon className="py-1 h-6 w-4 ml-1" />
+                ) : (
+                  <span />
+                )}
+              </li>
+            );
+          })}
+        </ul>
 
-            <Button
-              headless
-              className="font-mono my-3"
-              onClick={() => {
-                navigator.clipboard.writeText(url);
-                setUrlCopied(true);
+        <UserDisplay />
+      </Panel>
 
-                setTimeout(() => {
-                  setUrlCopied(false);
-                }, 3000);
-              }}
-            >
-              <p>{url}</p>
+      {/* Video */}
+      <div
+        id="video"
+        className="relative w-[-webkit-fill-available] h-screen flex items-center justify-center"
+      >
+        {/* Player */}
+        <div className="video-max-width mx-auto w-[inherit]" />
 
-              <p
-                className={`${
-                  urlCopied
-                    ? "text-light-accent-400 dark:text-dark-accent-400"
-                    : "text-light-secondary-500 dark:text-dark-secondary-500"
-                }`}
-              >
-                {urlCopied ? "copied!" : "click to copy"}
-              </p>
-            </Button>
-
-            <SpaceBetween>
-              <div />
-              <Button
-                onClick={() => {
-                  panel.setIsOpen(true);
-                  setIsDialogOpen(false);
-                }}
-                colorless
-              >
-                Done
-              </Button>
-            </SpaceBetween>
-          </SimpleDialog>
-
-          <ul className="h-full">
-            {getRoom.data?.users.map((user: any) => {
-              return (
-                <li key={user.ID} className="pb-1 flex align-middle">
-                  {user.name}
-                  {user.ID === getRoom.data?.ownerID ? (
-                    <StarIcon className="py-1 h-6 w-4 ml-1" />
-                  ) : (
-                    <span />
-                  )}
-                </li>
-              );
-            })}
-          </ul>
-
-          <UserDisplay />
-        </Panel>
-
-        {/* Video */}
-        <div
-          id="video"
-          className="relative w-[-webkit-fill-available] h-screen flex items-center justify-center"
-        >
-          {/* Player */}
-          <div className="video-max-width mx-auto w-[inherit]" />
-
-          {/* Menus */}
-          <OverlayMenu panel={panel} />
-        </div>
+        {/* Menus */}
+        <OverlayMenu panel={panel} />
       </div>
     </div>
   );
