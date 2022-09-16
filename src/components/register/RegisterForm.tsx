7ef63c3f9228e4e3812c0a3ce8fe0fd53369a423
Vazanoir <vazanoir@outlook.fr>
2022-09-16 19:15:16 +0200

    Fix bad import

--- src/components/register/RegisterForm.tsx
+++ src/components/register/RegisterForm.tsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { UserContext, ActionTypes as UserActionTypes } from "../../states/user";
+import { UserContext } from "../../states/user";
 import useCreateUser from "../../states/user/createUser";
 import Button from "../Button";
 import Input from "../Input";
